07edb371b880505939792ae4e510c6bac63c917c
Edward R. Rivera Rivera <77991221+rivera-squared@users.noreply.github.com>
2023-03-10 10:41:06 -0500

    Add files via upload

--- vocero/vocero_gobierno.py
+++ vocero/vocero_gobierno.py
@@ -9,43 +9,41 @@ def get_complete_link(enlace):
     vocero = 'https://www.elvocero.com'
     return vocero + enlace
 
-def get_link_extension(enlace):
+def get_title1():
+    enlace = 'https://www.elvocero.com/gobierno/'
     sel=Selector(text=requests.get(enlace).content)
-    link_extension = sel.xpath('//h3[@class="tnt-headline "]/a/@href').extract()
-    
-    enlace_completo=[]
-    for link in link_extension:
-        enlace_completo.append(get_complete_link(link))
-        
-    return enlace_completo
+    titles = sel.xpath('//h2[@class="tnt-headline "]/a/text()').extract()
+    title_clean = []
+    for title in titles:
+        title_clean.append(title.strip())
+    return title_clean
 
-
-def get_title(enlace):
-    
+def get_title2():
+    enlace = 'https://www.elvocero.com/gobierno/'
     sel=Selector(text=requests.get(enlace).content)
     titles = sel.xpath('//h3[@class="tnt-headline "]/a/text()').extract()
-    
-    titulo=[]
+    title_clean = []
     for title in titles:
-        titulo.append(title.strip())
-    
-    return titulo                      
-
-def get_author(enlace):
+        title_clean.append(title.strip())
+    return title_clean
+        
+def get_enlace1():
+    enlace = 'https://www.elvocero.com/gobierno/'
     sel=Selector(text=requests.get(enlace).content)
-    return ''.join(sel.xpath('//ul[@class="list-inline"]/li/span/a/text()').extract())
+    enlaces = sel.xpath('//h2[@class="tnt-headline "]/a/@href').extract()
+    enlace_completo = []
+    for x in enlaces:
+        enlace_completo.append(get_complete_link(x))
+    return enlace_completo
 
-def clean_autor(autor):
-    x = re.sub("(EL VOCERO)","", autor)
-    y = re.sub("[>,]","", x)
-    z = re.sub("/"," ", y)
-    
-    return z
-
-def get_fecha(enlace):
-    # enlace = 'https://www.elvocero.com/ley-y-orden/federal/relevan-de-sus-funciones-a-oficial-de-guardia-costera-que-choc-en-costas-de-dorado/article_00361b44-af8f-11ed-b3bb-0fe5f7a6525f.html'
+def get_enlace2():
+    enlace = 'https://www.elvocero.com/gobierno/'
     sel=Selector(text=requests.get(enlace).content)
-    return ''.join(sel.xpath('//li[@class="hidden-print"]/time/text()').extract())
+    enlaces = sel.xpath('//h3[@class="tnt-headline "]/a/@href').extract()
+    enlace_completo = []
+    for x in enlaces:
+        enlace_completo.append(get_complete_link(x))
+    return enlace_completo
 
 def clean_fecha(fecha):
     fecha = ''.join(re.findall("\d+/\d+/\d+", str(fecha)))   
@@ -53,75 +51,102 @@ def clean_fecha(fecha):
     if fecha =='':
         return "No fecha"
     else:
-        return ''.join(re.findall("\d+/\d+/\d+", str(fecha)))   
+        return ''.join(re.findall("\d+/\d+/\d+", str(fecha))) 
 
-def clean_fecha1(fecha):
-       
-    if fecha != 'No fecha':
-        return pd.to_datetime(fecha, dayfirst=True)
+def get_fecha(enlace):
+    sel=Selector(text=requests.get(enlace).content)
+    fecha = ''.join(sel.xpath('//li[@class="hidden-print"]/time/text()').extract())
+        
+    if fecha == '':
+        return np.nan
     else:
-        return np.nan              
+        return pd.to_datetime(clean_fecha(fecha), dayfirst=True)
+def clean_autor(autor):
+    x = re.sub("(EL VOCERO)","", autor)
+    y = re.sub("[>,]","", x)
+    z = re.sub("/"," ", y)
+    
+    return z
 
-def get_categoria(enlace):
-    x=re.sub('(https://www.elvocero.com/gobierno/)','',enlace)
-    y=re.sub('/\S+','',x)
-    return y.title()     
+def get_autor(enlace):
+    # enlace = 'https://www.elvocero.com/gobierno/fortaleza/gobernador-llega-a-bayam-n-para-reuni-n-con-la-federaci-n-de-alcaldes/article_761c9616-b774-11ed-b67e-7f8e07bfc2c7.html'
+    sel=Selector(text=requests.get(enlace).content)
+    autor = ''.join(sel.xpath('//ul[@class="list-inline"]/li/span/a/text()').extract())
+    
+    if autor == '':
+        return clean_autor(sel.xpath('//ul[@class="list-inline"]/li/span/text()').extract()[1]).strip()
+    elif autor == '':
+        return "No autor(a)"
+    else:
+        return clean_autor(autor).strip()    
 
-def get_noticias_vocero():
-    
-    enlaces = ['https://www.elvocero.com/gobierno/agencias/',
-                'https://www.elvocero.com/gobierno/fortaleza/',
-                'https://www.elvocero.com/gobierno/legislatura/',
-                'https://www.elvocero.com/gobierno/municipal/']
-    enlace=[]
-    titulo=[]
-    for link in enlaces:
-        
-        titulo.append(get_title(link))
-        enlace.append(get_link_extension(link))
-        
-        new_enlace = [x for sublist in enlace for x in sublist]
-        new_titulo = [x for sublist in titulo for x in sublist]
-        
-    vocero = pd.DataFrame({
-        "titulo":new_titulo,
-        "enlace":new_enlace})
-    
-    autor =[]
-    for enlace in vocero['enlace']:
-        autor.append(get_author(enlace))
-        
-    vocero['autor'] = autor
-    vocero['autor'] = vocero['autor'].apply(clean_autor)
-    
-    fechas = vocero['enlace'].apply(get_fecha)
-    vocero['fecha'] = fechas
-    vocero['fecha'] = vocero['fecha'].apply(clean_fecha)
-    vocero['fecha'] = vocero['fecha'].apply(clean_fecha1)
-    vocero['categoria']=vocero['enlace'].apply(get_categoria)
-    vocero=vocero[['fecha','titulo','autor','categoria','enlace']]
-    
-    
-    return vocero
 
+# Seccion principal
+def get_vocero_gobierno1():
+    titulo1 = get_title1()
+    enlace1 = get_enlace1()
+    vocero_gobierno = pd.DataFrame({
+        'titulo':titulo1,
+        'enlace':enlace1,
+        'categoria': "Gobierno"
+        })
+    vocero_gobierno['fecha'] = vocero_gobierno['enlace'].apply(get_fecha)
+    vocero_gobierno['autor(a)'] = vocero_gobierno['enlace'].apply(get_autor)
+    vocero_gobierno = vocero_gobierno[['fecha','titulo','autor(a)','categoria','enlace']]
+    vocero_gobierno = vocero_gobierno.sort_values(by='fecha', ascending = False)
+    
+    return vocero_gobierno
+
+# Seccion secundaria
+def get_vocero_gobierno2():
+    
+    titulo2 = get_title2()
+    enlace2 = get_enlace2()
+    vocero_gobierno = pd.DataFrame({
+        'titulo':titulo2,
+        'enlace':enlace2,
+        'categoria': "Gobierno"
+        })
+    vocero_gobierno['fecha'] = vocero_gobierno['enlace'].apply(get_fecha)
+    vocero_gobierno['autor(a)'] = vocero_gobierno['enlace'].apply(get_autor)
+    vocero_gobierno = vocero_gobierno[['fecha','titulo','autor(a)','categoria','enlace']]
+    vocero_gobierno = vocero_gobierno.sort_values(by='fecha', ascending = False)
+    
+    return vocero_gobierno
+
+def vocero_gobierno():
+    vocero_gobierno_principal = get_vocero_gobierno1()
+    vocero_gobierno_secundario = get_vocero_gobierno2()
+    vocero_gobierno = pd.concat([vocero_gobierno_principal, vocero_gobierno_secundario])
+    vocero_gobierno = vocero_gobierno.sort_values(by = 'fecha', ascending = False)
+    return vocero_gobierno
+
+# =============================================================================
+# Aplicar algoritmos
+# =============================================================================
 before = datetime.now()
 current_time = before.strftime("%H:%M:%S")
-print("Hora que comenzo a correr el algoritmo: {}".format(current_time))
+print("\nHora que comenzo a correr el algoritmo: {}".format(current_time))
       
 gobierno = pd.read_csv('vocero_gobierno.csv')
-gobierno['fecha'] = pd.to_datetime(gobierno['fecha'])
 
 antes=len(gobierno)    
-gobierno_new=get_noticias_vocero()
+print("Comenzo con {} articulos.".format(antes))
+
+gobierno_new = vocero_gobierno()
 gobierno = pd.concat([gobierno_new, gobierno])
 gobierno = gobierno.drop_duplicates(subset='enlace')
-
+gobierno['fecha'] = pd.to_datetime(gobierno['fecha'])
+gobierno = gobierno.sort_values(by = 'fecha', ascending = False)
 gobierno.to_csv('vocero_gobierno.csv',index=False)
 despues = len(gobierno)
 
 now = datetime.now()
 current_time1 = now.strftime("%H:%M:%S")
 print("Hora que termino de correr el algoritmo: {}".format(current_time1))
-print("\nComenzo con {} articulos.".format(antes))
+
 print("Termino con {} articulos.".format(despues))
 print("{} Articulos a~adidos luego de correr el algoritmo.".format(despues-antes))
+
+
+
